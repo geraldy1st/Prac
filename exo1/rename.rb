76450bda11fa016978a3b6d80683e4b8443f77d7
ZCLMK <zacharie.NTC@gmail.com>
2018-04-08 14:36:47 +0200

    ajout fichier test

---
+++ exo1/rename.rb
@@ -0,0 +1,14 @@
+# mon_chiffre = 15
+#
+# def augmenter(nbr)
+#   compteur = 1
+#   resultat = 0
+#
+#   while compteur < nbr
+#     resultat = resultat + compteur
+#     compteur = compteur + 1
+#   end
+#   puts resultat
+# end
+#
+# augmenter(mon_chiffre)
